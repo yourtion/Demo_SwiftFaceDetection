b7ac6b19bf92cc6a1db323379b3f2294fcd804ba
YourtionGuo <yourtion@gmail.com>
2015-11-12 15:09:10 +0800

    init Project

---
+++ SwiftFaceDetection/SwiftFaceDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFaceDetection
+//
+//  Created by YourtionGuo on 11/11/15.
+//  Copyright © 2015 Yourtion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
